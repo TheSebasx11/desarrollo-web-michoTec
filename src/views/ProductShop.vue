e43cc3747e8c63f1e3af827ec5d5f46e6e71c5e7
micho137 <michaenrangelmr@gmail.com>
2022-02-23 13:04:54 -0500

    ProductShop
    postdata: No sirve el tailwind en la que dijiste que si

--- src/views/ProductShop.vue
+++ src/views/ProductShop.vue
@@ -1,6 +1,177 @@
 <template>
-    <div class="bg-amber-300 w-96 border border-black h-96 flex items-center justify-center">
-      <p>aqui el maicol</p>
+    <div class="bg-slate-200 w-96 border border-black h-96 flex items-center justify-center">
+      <section>
+        <article >
+            <div class="all_featured_products m-5">
+                <h2 class="featured_products_container font-bold text-3xl text-center m-5">Productos destacados</h2>
+                <div class="grid grid-cols-3">
+                    <p>
+                    </p>
+                    <p class="text-center mb-10">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea nihil accusamus earum eius magnam quisquam incidunt error eaque iure soluta? Recusandae autem exercitationem pariatur labore magnam velit provident aliquid at.
+                    </p>
+                    <p>
+                    </p>
+                </div>
+                <div class="featured_products grid grid-cols-4 gap-10 mb-24">
+                    <div class="product_item_featured">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" />
+                        <p class="product_name text-center text-white font-semibold">CATEGORY</p>
+                    </div>
+                    <div class="product_item_featured">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" />
+                        <p class="product_name text-center text-white font-semibold">CATEGORY</p>
+                    </div>
+                    <div class="product_item_featured">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" />
+                        <p class="product_name text-center text-white font-semibold">CATEGORY</p>
+                    </div>
+                    <div class="product_item_featured">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" />
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+    <section class="flex">
+        <section class="">
+            <div class="announce_container my-6 ml-6">
+                <h2 class="text-3xl font-bold mb-7">Compra ya tu producto!</h2>
+                <p class="text-left mr-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea nihil accusamus earum eius magnam quisquam incidunt error eaque iure soluta? Recusandae autem exercitationem pariatur labore magnam velit provident aliquid at.
+                </p>
+            </div>
+            <div class="button_browse m-6">
+                <button class="border-2 border-white px-4 py-2 rounded-xl text-white font-semibold bg-blue-600 hover:scale-110 hover:bg-blue-900">
+                    Browse All
+                </button>
+            </div>
+        </section>
+        <section class="inline-grid grid-cols-2">
+            <div class="products_container">
+                <div class="firts_column mr-6">
+                    <div class="product_item_list grid grid-cols-2 mb-4">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" alt="" />
+                        <div class="photo_info_title items-center justify-center flex flex-col">
+                            <h2 class="photo_product_title text-xl font-sans font-bold text-center">
+                                Titulo de producto
+                            </h2>
+                            <div class="article_stars flex items-center justify-center m-1">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+    
+                            </div>
+                            <p class="article_review text-lg text-center">
+                                $5000
+                            </p>
+                        </div>
+                    </div>
+                    <div class="product_item_list grid grid-cols-2 mb-4">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" alt=""/>
+                        <div class="photo_info_title items-center justify-center flex flex-col">
+                            <h2 class="photo_product_title text-xl font-sans font-bold text-center">
+                                Titulo de producto
+                            </h2>
+                            <div class="article_stars flex items-center justify-center m-1">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+    
+                            </div>
+                            <p class="article_review text-lg text-center">
+                                $5000
+                            </p>
+                        </div>
+                    </div>
+                    <div class="product_item_list grid grid-cols-2 mb-4">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" alt="" />
+                        <div class="photo_info_title items-center justify-center flex flex-col">
+                            <h2 class="photo_product_title text-xl font-sans font-bold text-center">
+                                Titulo de producto
+                            </h2>
+                            <div class="article_stars flex items-center justify-center m-1">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+    
+                            </div>
+                            <p class="article_review text-lg text-center">
+                                $5000
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="second_container">
+                <div class="second_column mr-6">
+                    <div class="product_item_list grid grid-cols-2 mb-4">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" alt="" />
+                        <div class="photo_info_title items-center justify-center flex flex-col">
+                            <h2 class="photo_product_title text-xl font-sans font-bold text-center">
+                                Titulo de producto
+                            </h2>
+                            <div class="article_stars flex items-center justify-center m-1">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+    
+                            </div>
+                            <p class="article_review text-lg text-center">
+                                $5000
+                            </p>
+                        </div>
+                    </div>
+                    <div class="product_item_list grid grid-cols-2 mb-4">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" alt="" />
+                        <div class="photo_info_title items-center justify-center flex flex-col">
+                            <h2 class="photo_product_title text-xl font-sans font-bold text-center">
+                                Titulo de producto
+                            </h2>
+                            <div class="article_stars flex items-center justify-center m-1">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+    
+                            </div>
+                            <p class="article_review text-lg text-center">
+                                $5000
+                            </p>
+                        </div>
+                    </div>
+                    <div class="product_item_list grid grid-cols-2 mb-4">
+                        <img class="border-2 border-white" src="https://www.interfaz.com.mx/wp-content/uploads/2020/07/600x400.png" alt="" />
+                        <div class="photo_info_title items-center justify-center flex flex-col">
+                            <h2 class="photo_product_title text-xl font-sans font-bold text-center">
+                                Titulo de producto
+                            </h2>
+                            <div class="article_stars flex items-center justify-center m-1">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-3.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+                                <img class="star_small" src="../Imagenes producto/iconmonstr-star-thin.svg">
+    
+                            </div>
+                            <p class="article_review text-lg text-center">
+                                $5000
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
     </div>
 </template>
 
--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <div>Contenido</div>
+      <div class="m-5">
+                <h2 class="featured_products_container font-bold text-3xl text-center m-5">Productos destacados</h2>
+                <div class="grid grid-cols-3">
+                    <p>
+                    </p>
+                    <p class="text-center mb-10">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea nihil accusamus earum eius magnam quisquam incidunt error eaque iure soluta? Recusandae autem exercitationem pariatur labore magnam velit provident aliquid at.
+                    </p>
+                    <p>
+                    </p>
+      </div>
   </div>
 </template>
 
